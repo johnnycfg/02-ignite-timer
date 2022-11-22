5710d8a526fdf4b5c296e3510757c52e0e253c20
Johnny Ferraz <johnny.chr@hotmail.com>
2022-11-21 22:05:59 -0300

    some adjusts and header created

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+import { HeaderContainer } from './styles'
+
+import logoIgnite from '../../assets/logo-ignite.svg'
+import { Scroll, Timer } from 'phosphor-react'
+import { NavLink } from 'react-router-dom'
+
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoIgnite} alt="logo do ignite que é composta por 2 triângulos sobrepostos inclinados para a direita" />
+      <nav>
+        <NavLink to="/" title="Timer">
+          <Timer size={20} />
+        </NavLink>
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={20} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  )
+}
